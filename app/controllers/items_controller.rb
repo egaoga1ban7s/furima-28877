81cb7e159f1ac658d5c9528b3f35e40db3456b32
egaoga1ban7s <69257704+egaoga1ban7s@users.noreply.github.com>
2020-09-22 18:56:59 +0900

    IF修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
 
   def update
-    iif @item.update(item_params)
+    if @item.update(item_params)
     redirect_to root_path
   else
     render :edit
