8868aeab569fed75e864f10d2739fc6ad245a8c0
egaoga1ban7s <69257704+egaoga1ban7s@users.noreply.github.com>
2020-09-28 15:10:38 +0900

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,5 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 end
